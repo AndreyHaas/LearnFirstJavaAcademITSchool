ed9b5bf6ac2d4fe6d5a81f527cf2979a89e8fded
AndreyHaas <32494303+AndreyHaas@users.noreply.github.com>
2018-02-05 23:23:20 +0700

    Выложил проект на GitHub, подправил задачу по расчёту площади и радиуса круга в части форматированного вывода значения параметров до сотых.

--- src/Lecture/Two/CircleAreaCalculation.java
+++ src/Lecture/Two/CircleAreaCalculation.java
@@ -6,7 +6,7 @@ public class CircleAreaCalculation {
         double area = Math.PI * Math.pow(radius, 2);
         double circumference = 2 * Math.PI * radius;
 
-        System.out.println("Площадь окружности при заданном радиусе равна: " + area);
-        System.out.println("Длина окружности при заданном радиусе равна: " + circumference);
+        System.out.printf("Площадь окружности при заданном радиусе равна: %.2f%n", area);
+        System.out.printf("Длина окружности при заданном радиусе равна: %.2f%n", circumference);
     }
 }
