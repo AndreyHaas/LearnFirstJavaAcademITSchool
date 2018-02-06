bfaafdb7642617635787e14fe542d4ecb39764d7
AndreyHaas <32494303+AndreyHaas@users.noreply.github.com>
2018-02-06 23:30:42 +0700

    Доделал 1 задачу 3-й лекции про if

---
+++ src/Lecture/Three/EvenNumbers.java
@@ -0,0 +1,20 @@
+package Lecture.Three;
+
+import java.util.Scanner;
+
+public class EvenNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int digit = scanner.nextInt();
+
+        if (digit > 0)
+            System.out.println("Введёное вами число положительное!");
+
+        if (digit % 2 == 0)
+            System.out.println("Введёное вами число чётное!");
+
+        if (digit % 5 == 0)
+            System.out.println("Введёное вами кратно пяти!");
+    }
+}
