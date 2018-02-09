78b97565094284e4082e908e1ce86c2e9df89698
AndreyHaas <32494303+AndreyHaas@users.noreply.github.com>
2018-02-10 00:21:34 +0700

    Доделал задачу на расчёт високосного года.

--- src/Lecture/Three/LeapYear.java
+++ src/Lecture/Three/LeapYear.java
@@ -1,7 +1,20 @@
 package Lecture.Three;
 
+//лекция 3 страница 35
+
+import java.util.Scanner;
+
 public class LeapYear {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Введите 4 цифры года для проверки, является ли он високосным: ");
+        int year = scanner.nextInt();
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println("Введённый вами год - високосный.");
+        } else {
+            System.out.println("Введённый вами год - НЕ високосный.");
+        }
     }
 }
