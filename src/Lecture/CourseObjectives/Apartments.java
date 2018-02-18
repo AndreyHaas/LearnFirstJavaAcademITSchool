df03ab9d628dbac6676f85ebb77f7d2f07c772a2
AndreyHaas <32494303+AndreyHaas@users.noreply.github.com>
2018-02-18 23:36:05 +0700

    Залил задачу Apartments.

--- src/Lecture/CourseObjectives/Apartments.java
+++ src/Lecture/CourseObjectives/Apartments.java
@@ -5,28 +5,20 @@ import java.util.Scanner;
 public class Apartments {
     public static void main(String[] args) {
 
-        int countOfApartments;
-        int totalEntrances;
-        int totalFloors;
-        int enteredApartmentNumber;
-        int apartmentsOnFloor;
-        int entrance;
-        int floor;
-        int location;
-        String decodingLocation;
-
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Введите количество подъездов в многоквартирном доме:");
-        totalEntrances = scanner.nextInt();
+        int totalEntrances = scanner.nextInt();
 
         System.out.println("Введите количество этажей в многоквартирном доме:");
-        totalFloors = scanner.nextInt();
+        int totalFloors = scanner.nextInt();
 
         System.out.println("Введите номер искомой квартиры в многоквартирном доме:");
-        enteredApartmentNumber = scanner.nextInt();
+        int enteredApartmentNumber = scanner.nextInt();
 
-        apartmentsOnFloor = 4;
+        int apartmentsOnFloor = 4;
+
+        int countOfApartments;
         countOfApartments = totalEntrances * totalFloors * apartmentsOnFloor;
 
         if (enteredApartmentNumber <= 0 || enteredApartmentNumber > countOfApartments) {
@@ -47,10 +39,12 @@ public class Apartments {
 //            }
 //        }
 //
-        entrance = (enteredApartmentNumber - 1) / (totalFloors * apartmentsOnFloor) + 1;
-        floor = ((enteredApartmentNumber - 1) % (totalFloors * apartmentsOnFloor)) / apartmentsOnFloor + 1;
+        int entrance = (enteredApartmentNumber - 1) / (totalFloors * apartmentsOnFloor) + 1;
+        int floor = ((enteredApartmentNumber - 1) % (totalFloors * apartmentsOnFloor)) / apartmentsOnFloor + 1;
 
-        location = (enteredApartmentNumber - 1) % apartmentsOnFloor;
+        String decodingLocation;
+        int location = (enteredApartmentNumber - 1) % apartmentsOnFloor;
+
         if (location == 0) {
             decodingLocation = "ближняя слева.";
         } else if (location == 1) {
