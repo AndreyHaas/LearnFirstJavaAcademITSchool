db263b294213eac6321ea86a0e4e67b80e66f1cf
AndreyHaas <32494303+AndreyHaas@users.noreply.github.com>
2018-02-18 23:24:57 +0700

    Залил задачу theAge.

--- src/Lecture/CourseObjectives/TheAge.java
+++ src/Lecture/CourseObjectives/TheAge.java
@@ -8,30 +8,46 @@ import java.util.Scanner;
 
 public class TheAge {
     public static void main(String[] args) {
-        String ageDescription;
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Введите ваш возраст в диапазоне от 1 до 112: ");
         int age = scanner.nextInt();
+// прямой способ, как вижу, так и читаю:
+//        if (age == 1 || age == 21 || age == 31 || age == 41 || age == 51 || age == 61 || age == 71 || age == 81 || age == 91 || age == 101) {
+//            ageDescription = "год.";
+//        } else if (
+//                age >= 2 && age <= 4    || age >= 22 && age <= 24   || age >= 32 && age <= 34 || age >= 42 && age <= 44 ||
+//                age >= 52 && age <= 54  || age >= 62 && age <= 64   || age >= 72 && age <= 74 || age >= 82 && age <= 84 ||
+//                age >= 92 && age <= 94  || age >= 102 && age <= 104)
+//        {
+//            ageDescription = "года.";
+//        } else {
+//            ageDescription = "лет.";
+//        }
+        int ageLastNumber = age % 10;
+        boolean isExclusion = (age % 100 >= 11) && (age % 100 <= 14);
+        String old = "";
 
-        if (age == 1 || age == 21 || age == 31 || age == 41 || age == 51 || age == 61 || age == 71 || age == 81 || age == 91 || age == 101) {
-            ageDescription = "год.";
-        } else if (
-                age >= 2 && age <= 4    || age >= 22 && age <= 24   || age >= 32 && age <= 34 || age >= 42 && age <= 44 ||
-                age >= 52 && age <= 54  || age >= 62 && age <= 64   || age >= 72 && age <= 74 || age >= 82 && age <= 84 ||
-                age >= 92 && age <= 94  || age >= 102 && age <= 104)
-        {
-            ageDescription = "года.";
-        } else {
-            ageDescription = "лет.";
-        }
-
-        if (age >= 1 && age <= 112) {
-            System.out.println("Вам " + age + " " + ageDescription);
-        } else if (age < 1) {
+        if (age < 1) {
             System.out.println("Вы слишком малы.");
-        } else {
+            return;
+        } else if (age > 112) {
             System.out.println("Вы слишком стары.");
+            return;
         }
+
+        if (ageLastNumber == 1) {
+            old = "год";
+        } else if (ageLastNumber == 0 || ageLastNumber >= 5 && ageLastNumber <= 9) {
+            old = "лет";
+        } else if (ageLastNumber >= 2 && ageLastNumber <= 4) {
+            old = "года";
+        }
+        if (isExclusion) {
+            old = "лет";
+        }
+
+        System.out.println("Вам " + age + " " + old);
     }
 }
