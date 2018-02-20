b589b66ea683da56e01009379ca3007237c3cbf0
AndreyHaas <32494303+AndreyHaas@users.noreply.github.com>
2018-02-21 00:07:38 +0700

    Залил задачу AreaOfTriangle.

--- src/Lecture/CourseObjectives/AreaOfTriangle.java
+++ src/Lecture/CourseObjectives/AreaOfTriangle.java
@@ -9,32 +9,36 @@ import java.util.Scanner;
 public class AreaOfTriangle {
     public static void main(String[] args) {
 
-        double x1, y1, x2, y2, x3, y3, area;
-
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Введите координату x1 для первой вершины треугольника: ");
-        x1 = scanner.nextInt();
+        double x1 = scanner.nextInt();
         System.out.println("Введите координату y1 для первой вершины треугольника: ");
-        y1 = scanner.nextInt();
+        double y1 = scanner.nextInt();
 
         System.out.println("Введите значение x2 для второй вершины треугольника: ");
-        x2 = scanner.nextInt();
+        double x2 = scanner.nextInt();
         System.out.println("Введите значение y2 для второй вершины треугольника: ");
-        y2 = scanner.nextInt();
+        double y2 = scanner.nextInt();
 
         System.out.println("Введите значение x3 для третьей вершины треугольника: ");
-        x3 = scanner.nextInt();
+        double x3 = scanner.nextInt();
         System.out.println("Введите значение y3 для третьей вершины треугольника: ");
-        y3 = scanner.nextInt();
+        double y3 = scanner.nextInt();
 
-        area = Math.abs((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)) / 2;
+        // прямой расчёт площади треугольника (без применения формулы Герона - работает
+//        int area = Math.abs((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)) / 2;
 
+        double a = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+        double b = Math.sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
+        double c = Math.sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
+        double p = (a + b + c) / 2;
+
+        double area = Math.sqrt((p * (p - a) * (p - b) * (p - c)));
         if (area > 0) {
             System.out.println("Площадь треугольника равна " + area);
         } else {
-            System.out.println("Заданные вершины лежат на одной прямой!!!" +
-                    " Площадь треугольника в данном случае не вычисляется!!!");
+            System.out.println("Заданные вершины лежат на одной прямой!!!" + " Площадь треугольника в данном случае не вычисляется!!!");
         }
     }
 }
