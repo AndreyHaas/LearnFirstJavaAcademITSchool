b31d650f5af13b029a4825d69affb400d37d2fcf
AndreyHaas <32494303+AndreyHaas@users.noreply.github.com>
2018-02-21 21:05:41 +0700

    Залил задачу про SwitchCase.

--- src/Lecture/CourseObjectives/NextDay.java
+++ src/Lecture/CourseObjectives/NextDay.java
@@ -34,7 +34,7 @@ public class NextDay {
         }
 
         if ((month == 4 || month == 6 || month == 9 || month == 11) && day < 30) {
-            day++;
+            day += 1;
         } else if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && day < 31) {
             day += 1;
         }
---
+++ src/Lecture/Four/SwitchCase.java
@@ -0,0 +1,28 @@
+package Lecture.Four;
+
+import java.util.Scanner;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+        Scanner command = new Scanner((System.in));
+
+        System.out.println("Введите команду: ");
+        String value = command.nextLine();
+        switch (value) {
+            case "print":
+                System.out.println("Введите строку: ");
+                String read = command.nextLine();
+                System.out.println(read);
+                break;
+            case "sum":
+                System.out.println("Введите вещественное число: ");
+                double firstNumber = command.nextDouble();
+                System.out.println("Введите вещественное число: ");
+                double nextNumber = command.nextDouble();
+                System.out.printf("Sum = %f", firstNumber + nextNumber);
+                break;
+            default:
+                System.out.println("Unknown command.");
+        }
+    }
+}
