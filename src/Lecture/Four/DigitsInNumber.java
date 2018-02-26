81bdf7de67187f3d7702a38cf1b2b66e58a346bb
AndreyHaas <32494303+AndreyHaas@users.noreply.github.com>
2018-02-26 23:24:19 +0700

    Слил решения.

--- src/Lecture/Four/DigitsInNumber.java
+++ src/Lecture/Four/DigitsInNumber.java
@@ -6,14 +6,11 @@ public class DigitsInNumber {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
         int a, count = 0;
         int odd = 0;
-
         if (scanner.hasNextInt()) {
             System.out.println("Введите целое число: ");
             a = scanner.nextInt();
-
             while (a != 0) {
                 count = count + a % 10;
                 a /= 10;
