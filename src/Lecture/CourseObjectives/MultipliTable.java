7a185db2b7de25f17c0ba48e69c4bfbf22f3291f
AndreyHaas <32494303+AndreyHaas@users.noreply.github.com>
2018-03-04 00:34:41 +0700

    Слил решения.

--- src/Lecture/CourseObjectives/MultipliTable.java
+++ src/Lecture/CourseObjectives/MultipliTable.java
@@ -5,14 +5,14 @@ package Lecture.CourseObjectives;
  */
 public class MultipliTable {
     public static void main(String[] args) {
-        System.out.printf("  * ||");
+        System.out.printf("   * ||");
         for (int i = 1; i <= 10; i++)
-            System.out.printf("%3d ", i);
-        System.out.printf("%n====||=======================================%n");
+            System.out.printf("%4d ", i);
+        System.out.printf("%n=====||=================================================%n");
         for (int i = 1; i <= 10; i++) {
-            System.out.printf("%3d ||", i);
+            System.out.printf("%4d ||", i);
             for (int j = 1; j <= 10; j++)
-                System.out.printf("%3d ", i * j);
+                System.out.printf("%4d ", i * j);
             System.out.printf("%n");
         }
     }
